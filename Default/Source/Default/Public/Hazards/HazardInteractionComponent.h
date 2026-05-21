151c46683662e57ba8acbd7987b175f207ddfe30
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-21 10:46:10 +0600

    Add hazard interaction component to Default project

---
+++ Default/Source/Default/Public/Hazards/HazardInteractionComponent.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Training/TrainingTypes.h"
+#include "HazardInteractionComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHazardInteractionEvaluated, const FString&, HazardCode, bool, bWasCorrect);
+
+UCLASS(ClassGroup=(Training), meta=(BlueprintSpawnableComponent))
+class DEFAULT_API UHazardInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHazardInteractionComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Hazard")
+	bool MarkHazardFound(const FString& Action = TEXT("marked_hazard"));
+
+	UFUNCTION(BlueprintCallable, Category = "Hazard")
+	bool RecordUnsafeTrainingAction(const FString& Action = TEXT("unsafe_action"));
+
+	UFUNCTION(BlueprintPure, Category = "Hazard")
+	bool IsResolved() const;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	FString HazardCode;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	FString HazardCategory;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	EHazardSeverity Severity = EHazardSeverity::Minor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	bool bCritical = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hazard")
+	FString FeedbackLocalizationKey;
+
+	UPROPERTY(BlueprintAssignable, Category = "Hazard")
+	FOnHazardInteractionEvaluated OnHazardInteractionEvaluated;
+
+private:
+	bool bResolved = false;
+};
